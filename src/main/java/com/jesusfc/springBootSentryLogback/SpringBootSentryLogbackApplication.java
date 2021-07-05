ed0c81667462bdf575b04c877fe1a35714934298
jesusfc <jfcaraballo@gmail.com>
2021-07-05 15:25:19 +0200

    firts update for this project

---
+++ src/main/java/com/jesusfc/springBootSentryLogback/SpringBootSentryLogbackApplication.java
@@ -0,0 +1,13 @@
+package com.jesusfc.springBootSentryLogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSentryLogbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSentryLogbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesusfc/springBootSentryLogback/SpringBootSentryLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesusfc.springBootSentryLogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSentryLogbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
