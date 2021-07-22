1de5c3886b94be1c44b345a0a7306fcbffebb4b3
jesusfc <jfcaraballo@gmail.com>
2021-07-22 18:08:41 +0200

    Update for this project

--- src/main/java/com/jesusfc/springBootSentryLogback/config/SentryConfiguration.java
+++ src/main/java/com/jesusfc/springBootSentryLogback/config/SentryConfiguration.java
@@ -1,53 +1,37 @@
 package com.jesusfc.springBootSentryLogback.config;
 
 import io.sentry.Sentry;
-import io.sentry.SentryClient;
-import io.sentry.spring.SentryExceptionResolver;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-
-import javax.annotation.PostConstruct;
+import org.springframework.context.annotation.Profile;
 
 /**
  * Created By Jesús Fdez. Caraballo on 2021.
  */
 
 @Configuration
-@ConditionalOnProperty(value = "sentry.enable")
+@Profile("development")
+//@ConditionalOnProperty(value = "sentry.enable")
 public class SentryConfiguration {
 
+
     @Value("${sentry.dsn}")
-    String dsn;
+    private static String dsn;
 
     @Value("${sentry.environment}")
-    String environment;
+    private static String environment;
+
 
     @Value("${sentry.release}")
-    String release;
+    private String release;
 
-/*
-    public SentryConfiguration(){
-        sentryClient = Sentry.init(dsn);
-        sentryClient.setEnvironment(environment);
-        sentryClient.setRelease(release);
+    public SentryConfiguration() {
+        Sentry.init(options -> {
+            options.setDsn(dsn);
+            options.setEnvironment(environment);
+            options.setRelease(release);
+        });
+
     }
 
-    @Bean
-    @ConditionalOnMissingBean(SentryExceptionResolver.class)
-    public HandlerExceptionResolver sentryExceptionResolver() {
-        return new SentryExceptionResolver();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(SentryServletContextInitializer.class)
-    public ServletContextInitializer sentryServletContextInitializer() {
-        return new SentryServletContextInitializer();
-    }
-
- */
 }
--- src/main/java/com/jesusfc/springBootSentryLogback/controllers/IndexController.java
+++ src/main/java/com/jesusfc/springBootSentryLogback/controllers/IndexController.java
@@ -1,21 +1,24 @@
 package com.jesusfc.springBootSentryLogback.controllers;
 
 import io.sentry.Sentry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created By Jesús Fdez. Caraballo on 2021.
  */
+@Slf4j
 @Controller
 public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
+    public String getIndexPage() {
         try {
             String a = null;
             System.out.println(a.length());
         } catch (Exception ex) {
+            log.error("Error: " + ex);
             Sentry.captureException(ex);
         }
 
